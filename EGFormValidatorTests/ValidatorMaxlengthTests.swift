5bcf32bc2550a0243a9984d62fbc7fe3bebad1fb
Evgeny Gushchin <latenal@gmail.com>
2017-01-02 13:38:29 -0600

    added maxlength tests

--- EGFormValidatorTests/ValidatorMaxlengthTests.swift
+++ EGFormValidatorTests/ValidatorMaxlengthTests.swift
@@ -7,16 +7,41 @@
 //
 
 import XCTest
+@testable import EGFormValidator
 
 class ValidatorMaxlengthTests: XCTestCase {
+    var textField: UITextField!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        textField = UITextField()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    
+    func testDefaultValue() {
+        XCTAssertEqual(Int.max, textField.maxLength, "Default value is too short")
+    }
+    
+    
+    func testMaxlength() {
+        textField.maxLength = 5
+        textField.text = "qwe"
+        XCTAssertEqual(textField.text?.characters.count, 3)
+        
+        textField.maxLength = 5
+        textField.text = "qwert"
+        XCTAssertEqual(textField.text?.characters.count, 5)
+        
+        textField.maxLength = 5
+        textField.text = "qwerty"
+        XCTAssertEqual(textField.text?.characters.count, 5)
+        
+        XCTAssertEqual(textField.maxLength, 5)
     }
 }
