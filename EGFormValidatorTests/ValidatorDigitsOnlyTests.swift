72717da5975ff270d3c9b0dc8b5143f3f0414f1f
Evgeny Gushchin <latenal@gmail.com>
2017-01-02 14:06:32 -0600

    Added Validator DigitsOnly Tests

---
+++ EGFormValidatorTests/ValidatorDigitsOnlyTests.swift
@@ -0,0 +1,58 @@
+//
+//  ValidatorDigitsOnlyTests.swift
+//  EGFormValidator
+//
+//  Created by Evgeny Gushchin on 02/01/17.
+//  Copyright © 2017 Evgeny Gushchin. All rights reserved.
+//
+
+import XCTest
+@testable import EGFormValidator
+
+class ValidatorDigitsOnlyTests: XCTestCase {
+    
+    var vc: ValidatorViewController!
+    var textField: UITextField!
+    var placeholder: UILabel!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        vc = ValidatorViewController()
+        textField = UITextField()
+        placeholder = UILabel()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    func testValidator() {
+        vc.addValidatorDigitsOnly(toControl: textField, errorPlaceholder: placeholder, errorMessage: "Error")
+        XCTAssert(vc.validate(), "Field is empty")
+        
+        textField.text = ""
+        XCTAssert(vc.validate(), "Field is empty")
+        
+        textField.text = "abc"
+        XCTAssertFalse(vc.validate(), "Field has non-digits")
+        
+        textField.text = "AB,C"
+        XCTAssertFalse(vc.validate(), "Field has non-digits")
+        
+        textField.text = "01234567890"
+        XCTAssert(vc.validate(), "Field has only digits")
+        
+        textField.text = "000aaa"
+        XCTAssertFalse(vc.validate(), "Field has non-digits")
+        
+        textField.text = "0.5"
+        XCTAssertFalse(vc.validate(), "Field has non-digits")
+        
+        textField.text = "100,000"
+        XCTAssertFalse(vc.validate(), "Field has non-digits")
+    }
+    
+}
