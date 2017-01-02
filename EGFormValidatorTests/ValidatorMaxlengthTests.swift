cd462e8032929bafd86035fee2150165f5839aa9
Evgeny Gushchin <latenal@gmail.com>
2017-01-02 13:16:45 -0600

    Add Maxlength test file

---
+++ EGFormValidatorTests/ValidatorMaxlengthTests.swift
@@ -0,0 +1,22 @@
+//
+//  ValidatorMaxlengthTests.swift
+//  EGFormValidator
+//
+//  Created by Evgeny Gushchin on 02/01/17.
+//  Copyright © 2017 Evgeny Gushchin. All rights reserved.
+//
+
+import XCTest
+
+class ValidatorMaxlengthTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
