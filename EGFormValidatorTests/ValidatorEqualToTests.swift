b2370f3352bc3de16cb26d0e66401714b2ed1617
Evgeny Gushchin <latenal@gmail.com>
2017-01-02 14:38:28 -0600

    cover more types of EqualTo

--- EGFormValidatorTests/ValidatorEqualToTests.swift
+++ EGFormValidatorTests/ValidatorEqualToTests.swift
@@ -31,8 +31,11 @@ class ValidatorEqualToTests: XCTestCase {
     }
     
     
-    func testValidator() {
+    func testValidator0() {
         vc.addValidatorEqualTo(toControl: textField1, errorPlaceholder: placeholder, errorMessage: "ERROR", compareWithControl: textField2)
+        
+        textField1.text = nil
+        textField2.text = nil
         XCTAssert(vc.validate(), "Fields are equal")
         
         textField1.text = ""
@@ -52,4 +55,89 @@ class ValidatorEqualToTests: XCTestCase {
         XCTAssertFalse(vc.validate(), "Fields arn't equal")
     }
     
+    
+    func testValidator1() {
+        let customControl1 = CustomControl()
+        let customControl2 = CustomControl()
+        vc.addValidatorEqualTo(toControl: customControl1, errorPlaceholder: placeholder, errorMessage: "ERROR", compareWithControl: customControl2)
+        
+        customControl1.value = 10
+        customControl2.value = 10
+        XCTAssert(vc.validate(), "Fields are equal")
+        
+        customControl2.value = 20
+        XCTAssertFalse(vc.validate(), "Fields are not equal")
+    }
+    
+    
+    
+    func testValidator2() {
+        let customControl1 = CustomControlFloat()
+        let customControl2 = CustomControlFloat()
+        vc.addValidatorEqualTo(toControl: customControl1, errorPlaceholder: placeholder, errorMessage: "ERROR", compareWithControl: customControl2)
+        
+        customControl1.value = 10.0
+        customControl2.value = 10.0
+        XCTAssert(vc.validate(), "Fields are equal")
+        
+        customControl2.value = 20
+        XCTAssertFalse(vc.validate(), "Fields are not equal")
+    }
+    
+    
+    
+    func testValidator3() {
+        let customControl1 = CustomControlDouble()
+        let customControl2 = CustomControlDouble()
+        vc.addValidatorEqualTo(toControl: customControl1, errorPlaceholder: placeholder, errorMessage: "ERROR", compareWithControl: customControl2)
+        
+        customControl1.value = 10.0
+        customControl2.value = 10.0
+        XCTAssert(vc.validate(), "Fields are equal")
+        
+        customControl2.value = 20
+        XCTAssertFalse(vc.validate(), "Fields are not equal")
+    }
+    
+    
+    
+    func testValidator4() {
+        let customControl1 = CustomControlCharacter()
+        let customControl2 = CustomControlCharacter()
+        vc.addValidatorEqualTo(toControl: customControl1, errorPlaceholder: placeholder, errorMessage: "ERROR", compareWithControl: customControl2)
+        
+        customControl1.value = "a"
+        customControl2.value = "a"
+        XCTAssert(vc.validate(), "Fields are equal")
+        
+        customControl2.value = "A"
+        XCTAssertFalse(vc.validate(), "Fields are not equal")
+    }
+}
+
+
+
+class CustomControlFloat: UIView, Validatable {
+    var value: Float?
+    
+    func getValue() -> Any? {
+        return value
+    }
+}
+
+class CustomControlDouble: UIView, Validatable {
+    var value: Double?
+    
+    func getValue() -> Any? {
+        return value
+    }
+}
+
+
+class CustomControlCharacter: UIView, Validatable {
+    var value: Character?
+    
+    func getValue() -> Any? {
+        return value
+    }
 }
