7424879739803bb3f576456e77087e7dfaf05074
Evgeny Gushchin <latenal@gmail.com>
2017-01-02 11:19:33 -0600

    Added UILabel tests

---
+++ EGFormValidatorTests/UILabelTests.swift
@@ -0,0 +1,34 @@
+//
+//  UILabelTests.swift
+//  EGFormValidator
+//
+//  Created by Evgeny Gushchin on 02/01/17.
+//  Copyright © 2017 Evgeny Gushchin. All rights reserved.
+//
+
+import XCTest
+@testable import EGFormValidator
+
+class UILabelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testErrorDisplayable() {
+        let conforms = (UILabel.self as Any) is ValidationErrorDisplayable.Type
+        XCTAssert(conforms)
+    }
+    
+    func testMethod() {
+        let label = UILabel()
+        label.setErrorMessage(errorMessage: "Some message")
+        XCTAssertEqual(label.text, "Some message")        
+    }
+}
---
+++ EGFormValidatorTests/ValidatableTests.swift
@@ -0,0 +1,42 @@
+//
+//  ValidatableTests.swift
+//  EGFormValidator
+//
+//  Created by Evgeny Gushchin on 02/01/17.
+//  Copyright © 2017 Evgeny Gushchin. All rights reserved.
+//
+
+import XCTest
+
+class ValidatableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+ 
+    
+    func testRequiredMethod() {
+        let x = Validatable()
+        
+        
+        
+    }
+}
