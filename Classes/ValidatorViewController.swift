737bc308ece45c58069140422b951e372ab9e358
Evgeny Gushchin <latenal@gmail.com>
2017-01-04 16:36:20 -0600

    Added support for conditional validation

--- Classes/ValidatorViewController.swift
+++ Classes/ValidatorViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
+/// Declares validation condition
+public typealias ValidatorCondition = () -> Bool
+
 /// Adds to `UIViewController` validation functionality
 open class ValidatorViewController: UIViewController {
     // MARK - VALIDATION
     private var validators = [Validator]()
+    private var validatorConditions = [ValidatorCondition]()
     
     
     
     /**
-     Adds a new validator to a validation list
+     Adds a new unconditional validator to a validation list
      
      - Parameter validator: A validator to be added to the validation list
      
@@ -25,7 +29,26 @@ open class ValidatorViewController: UIViewController {
      It recommended to add validatots consequently respecting the order of a fields in the form.
      */
     public func add(validator: Validator) {
+        self.add(validator: validator) { () -> Bool in
+            return true
+        }
+    }
+    
+    
+    /**
+     Adds a new conditional validator to a validation list
+     
+     - Parameter validator: A validator to be added to the validation list
+     - Parameter condition: A condition when the validator must be executed
+     
+     The order of added validators matters. The first added validators will be executed first.
+     If a validator of a control fails, the other validators of the control will not be executed.
+     It recommended to add validatots consequently respecting the order of a fields in the form.
+     If condition returns `false` the validator will mark the field as valid.
+     */
+    public func add(validator: Validator, condition: @escaping ValidatorCondition) {
         validators.append(validator)
+        validatorConditions.append(condition)
     }
     
     
@@ -48,7 +71,7 @@ open class ValidatorViewController: UIViewController {
         var inValidatedControls = Set<UIView>()
         self.firstFailedControl = nil
         
-        for validator in validators {
+        for (index, validator) in validators.enumerated() {
             var isControlValid = false
             
             // all controls should be UIViews so there shouldn't be any problem
@@ -59,24 +82,33 @@ open class ValidatorViewController: UIViewController {
                     // the control is not validated yet
                     // or it has been already validated and it passed all its previus validations
                     
-                    // get validation result
-                    isControlValid = validator.validate()
-                    
-                    if isControlValid {
+                    // check conditions
+                    if validatorConditions[index]() {
+                        // get validation result
+                        isControlValid = validator.validate()
+                        
+                        if isControlValid {
+                            // control is valid
+                            validator.control?.setValidation?(state: .valid)
+                            validator.errorPlaceholder?.setErrorMessage(errorMessage: nil)
+                        } else {
+                            // control is not valid
+                            validator.control?.setValidation?(state: .error)
+                            validator.errorPlaceholder?.setErrorMessage(errorMessage: validator.errorMessage)
+                            
+                            // add the control to inValidatedControls set
+                            inValidatedControls.insert(control)
+                            
+                            if self.firstFailedControl == nil {
+                                self.firstFailedControl = control
+                            }
+                        }
+                    } else {
+                        // validator was executed
                         // control is valid
+                        isControlValid = true
                         validator.control?.setValidation?(state: .valid)
                         validator.errorPlaceholder?.setErrorMessage(errorMessage: nil)
-                    } else {
-                        // control is not valid
-                        validator.control?.setValidation?(state: .error)
-                        validator.errorPlaceholder?.setErrorMessage(errorMessage: validator.errorMessage)
-                        
-                        // add the control to inValidatedControls set
-                        inValidatedControls.insert(control)
-                        
-                        if self.firstFailedControl == nil {
-                            self.firstFailedControl = control
-                        }
                     }
                 }
             }
