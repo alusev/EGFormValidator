44cf30806119032da74a5f349de0d5641c2b56b9
Evgeny Gushchin <latenal@gmail.com>
2017-02-01 16:45:41 -0600

    Fixes code style issues

--- Example/EGFormValidatorExample/CustomTextfield.swift
+++ Example/EGFormValidatorExample/CustomTextfield.swift
@@ -11,7 +11,7 @@ import Foundation
 import EGFormValidator
 
 class CustomTextfield: UITextField, UITextFieldDelegate {
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         
