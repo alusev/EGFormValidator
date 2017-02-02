fe80e3fcbc61ab245646cde64cbd08bd557409ba
Evgeny Gushchin <latenal@gmail.com>
2017-02-01 21:11:40 -0600

    Code style update

--- Example/EGFormValidatorExample/CustomTextfield.swift
+++ Example/EGFormValidatorExample/CustomTextfield.swift
@@ -14,57 +14,46 @@ class CustomTextfield: UITextField, UITextFieldDelegate {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         xibSetup()
     }
-    
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+
         xibSetup()
     }
-    
-    
-    
-    
+
     func xibSetup() {
         font = .systemFont(ofSize: 18)
         layer.cornerRadius = 6
         layer.borderWidth = 2
-        
-        
+
         setInitialStyles()
     }
-    
-    
-    
-    
+
     // Initial styles
     func setInitialStyles() {
         backgroundColor = isEnabled ? .white : UIColor(red: 239.0/255.0, green: 239.0/255.0, blue: 239.0/255.0, alpha: 1.0)
         layer.borderColor = UIColor(red: 244.0/255.0, green: 177.0/255.0, blue: 61.0/255.0, alpha: 1.0).cgColor
         textColor = UIColor(red: 86.0/255.0, green: 86.0/255.0, blue: 86.0/255.0, alpha: 1.0)
     }
-    
-    
+
     // On-focus styles
     func setOnFocusStyles() {
         backgroundColor = UIColor(red: 239.0/255.0, green: 239.0/255.0, blue: 239.0/255.0, alpha: 1.0)
         layer.borderColor = UIColor(red: 0/255.0, green: 47.0/255.0, blue: 113.0/255.0, alpha: 1.0).cgColor
         textColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1.0)
     }
-    
-    
+
     // On-error styles
     func setErrorStyles() {
         backgroundColor = UIColor(red: 239.0/255.0, green: 158.0/255.0, blue: 158.0/255.0, alpha: 1.0)
         layer.borderColor = UIColor(red: 230.0/255.0, green: 47.0/255.0, blue: 44.0/255.0, alpha: 1.0).cgColor
         textColor = .white
     }
-    
-    
-    
+
+
     //
     // Set styles for disabled textfield
     override var isEnabled: Bool {
@@ -72,8 +61,8 @@ class CustomTextfield: UITextField, UITextFieldDelegate {
             self.backgroundColor = isEnabled ? .white : UIColor(red: 239.0/255.0, green: 239.0/255.0, blue: 239.0/255.0, alpha: 1.0)
         }
     }
-    
-    
+
+
     //
     // Highlight the textfield on focus
     override func becomeFirstResponder() -> Bool {
@@ -81,11 +70,11 @@ class CustomTextfield: UITextField, UITextFieldDelegate {
         if result {
             self.setOnFocusStyles()
         }
-        
+
         return result
     }
-    
-    
+
+
     //
     // Out of focus
     override func resignFirstResponder() -> Bool {
@@ -93,20 +82,11 @@ class CustomTextfield: UITextField, UITextFieldDelegate {
         if result {
             self.setInitialStyles()
         }
-        
+
         return result
     }
-    
-    
-    
-
-    
-    
 
 
-
-
-    
     //
     // MARK: - Validatable protocol implementation
     func setValidation(state: ValidatableControlState) {
