391d34eb747cdc9603bfae4abac5be71d9f4a545
Roberto Casula <rcasula3@gmail.com>
2018-05-11 14:43:52 +0200

    moved ValidatorController

---
+++ Classes/ValidatorController.swift
@@ -0,0 +1,26 @@
+//
+//  ValidatorController.swift
+//  EGFormValidator
+//
+//  Created by Roberto Casula on 11/05/18.
+//  Copyright © 2018 Evgeny Gushchin. All rights reserved.
+//
+
+import Foundation
+
+
+/// Declares validation condition
+public typealias ValidatorCondition = () -> Bool
+
+public protocol ValidatorController: class {
+    var validators: [Validator] { get set }
+    var validatorConditions: [ValidatorCondition] { get set }
+    
+    /// Read-only property that stores the first failed in case if you need to scroll up your view and to show the invalid control
+    var firstFailedControl: UIView? { get set }
+    
+    func add(validator: Validator)
+    func add(validator: Validator, condition: @escaping ValidatorCondition)
+    
+    func validate() -> Bool
+}
